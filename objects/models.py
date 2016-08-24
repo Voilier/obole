30c007abfeb10932248bab6a552e207dc61cdc88
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-08-24 18:24:48 +0200

    Add .travis.yml, requirements.txt, WIP models

--- objects/models.py
+++ objects/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Object(models.Model):
+    owner = models.ForeignKey(User)
+    disposer = models.ForeignKey(User)
+
+
+class ObjectPermissions(models.Model):
+    PERMISSIONS = (
+        (0, _('Can be lent before being returned.'))
+    )
+
+    object = models.ForeignKey(Object)
+    permission = models.PositiveSmallIntegerField(choices=PERMISSIONS)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
-from django.db import models
-
-# Create your models here.
+# from django.db import models
+# from django.contrib.auth.models import User as BaseUser
+#
+#
+# class User(BaseUser):
+#     nickname = models.CharField(max_length=255)
